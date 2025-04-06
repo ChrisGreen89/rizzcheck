9dd398cfe463a7af6844ab7f10b3c9ced528fa6b
Chris Green <chris@sumoquote.com>
2025-04-06 11:11:20 -0600

    fix asset prefix

--- marketing-site/next.config.ts
+++ marketing-site/next.config.ts
@@ -2,7 +2,7 @@ const nextConfig = {
   output: 'export', // 👈 Enables static export
   // Optional: if your repo is hosted in a subfolder (e.g. github.com/yourname/repo)
   basePath: '/rizzcheck',
-  assetPrefix: '/rizzcheck/',
+  assetPrefix: '/',
 };
 
 export default nextConfig;
